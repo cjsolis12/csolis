f6784509c758a425768b8cc8670bbcb87546d66c
cjsolis <cjelizondo12@gmail.com>
2023-09-25 18:29:04 -0500

    added projectdata and looped over for work page

--- fullstack-portfolio/app/work/page.js
+++ fullstack-portfolio/app/work/page.js
@@ -1,3 +1,17 @@
+import projectData from "@/public/ProjectData";
+
 export default function work() {
-    return <h1>Work Page!</h1>
-  }
+  return (
+    <>
+      <section className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        {projectData.map((project, index) => (
+          <>
+            <img src={project.img.src} alt={project.title} className="w-full" />
+            {/* <h2 className="text-lg font-semibold mt-2">{project.title}</h2>
+            <p className="text-gray-600 mt-2">{project.description}</p> */}
+          </>
+        ))}
+      </section>
+    </>
+  );
+}
