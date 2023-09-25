b59971ce82de2d79572fb66b35a6bc902671b0ea
cjsolis <cjelizondo12@gmail.com>
2023-09-25 13:05:55 -0500

    added navbar

--- fullstack-portfolio/app/contact/page.js
+++ fullstack-portfolio/app/contact/page.js
@@ -1,3 +1,8 @@
 export default function contact() {
-    return <h1>Hello, contact!</h1>
+    return (
+      <>
+      <h1>Contact Page</h1>
+      <button className="btn btn-primary">Send</button>
+      </>
+    )
   }
--- fullstack-portfolio/app/page.js
+++ fullstack-portfolio/app/page.js
@@ -1,7 +1,16 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import Navbar from './navbar/page'
 
 export default function Home() {
   return (
-   <main><h1>Hello World</h1></main>
+   <main>
+    <Navbar/>
+    <h1>Hello World</h1>
+   <Link href="/about">About Me</Link>
+   <Link href="/work">Work</Link>
+   <Link href="/skills">Skills</Link>
+   <Link href="/contact">Contact</Link>
+   </main>
   )
 }
