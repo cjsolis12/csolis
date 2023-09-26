3cd8256391531ce79d120ff2440b415a0a0cb93e
cjsolis <cjelizondo12@gmail.com>
2023-09-25 19:14:59 -0500

    styling flex container for work samples

--- fullstack-portfolio/app/work/page.js
+++ fullstack-portfolio/app/work/page.js
@@ -16,24 +16,38 @@ export default function work() {
 
   return (
     <>
-      <div className="m-4">
-        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projectData.map((project, index) => (
-            <>
-              <motion.img
-                src={project.img.src}
-                alt={project.title}
-                className="w-full h-full object-cover"
-                animate={
-                  hoveredIndex === index ? { opacity: 0.6 } : { opacity: 1 }
-                }
-                transition={{ duration: 0.5, delay: 0.3 }}
-              />
-              {/* <h2 className="text-lg font-semibold mt-2">{project.title}</h2>
-            <p className="text-gray-600 mt-2">{project.description}</p> */}
-            </>
-          ))}
-        </section>
+      <div className="py-6 md:py-12">
+        <div className="container px-4 mx-auto">
+          {/* Title and description */}
+          <div className="md:flex md:-mx-4">
+            <div className="px-4 md:w-10/12 xl:w-8/12 mx-auto text-center">
+              <h1 className="text-2xl md:text-4xl font-medium mb-4">My Work</h1>
+              <p class="text-xl">
+                Hover to see more detail about my projects and their links.
+              </p>
+            </div>
+          </div>
+          <div className="md:flex md:flex-wrap md:-mx-4 mt-6 pt-6 md:mt-12">
+            {projectData.map((project, index) => (
+              <div key={index} className="px-4 md:w-1/2 mb-4">
+                <motion.img
+                  src={project.img.src}
+                  alt={project.title}
+                  className="w-full h-full object-fit"
+                  animate={
+                    hoveredIndex === index
+                      ? { opacity: 0.6 }
+                      : { opacity: 1 }
+                  }
+                  transition={{ duration: 0.5, delay: 0.3 }}
+                  onMouseEnter={() => handleHoverStart(index)}
+                  onMouseLeave={handleHoverEnd}
+                />
+                <h2 className="text-lg font-semibold mt-2">{project.title}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
