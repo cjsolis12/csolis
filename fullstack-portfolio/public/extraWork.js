f91226c9839f48576afcac943185e52ee54e7d98
cjsolis <cjelizondo12@gmail.com>
2023-10-06 17:58:35 -0500

    added extra work file and components

---
+++ fullstack-portfolio/public/extraWork.js
@@ -0,0 +1,34 @@
+const extraWork = [
+    {
+      title: "Case Studies",
+      description:
+        "This project involved conducting interviews with our top clients to highlight how Documint has been beneficial to their work. I compiled the interview findings for publications on Documint's company website.",
+      link: "https://documint.me/case-study",
+    },
+    {
+      title: "Documentation",
+      description:
+        "This assignment provided an opportunity to leverage my teaching skills while simultaneously acquiring a comprehensive understanding of technical documentation. I restructured the layout for the pages and added documentation for new features.",
+      link: "https://docs.documint.me/",
+    },
+    {
+      title: "Password Generator",
+      description:
+        "Instantly generate a secure, random password with this tool. This app features dynamically updated HTML and CSS powered by Javascript code.",
+      link: "https://cjsolis12.github.io/Password_Generator/",
+    },
+    {
+      title: "Ecommerce Backend",
+      description:
+        "This node application uses express and MySQL to build a backend for an e-commerce site. Sequelize is used to connect and interact with the database. The schema is designed to link certain tables by id.",
+      link: "https://github.com/cjsolis12/ecommerce_backend/tree/main/E-Commerce",
+    },
+    {
+      title: "Work Day Schedular ",
+      description:
+        "This calendar application is built using HTML, CSS, jQuery, and the Day.js library. It enables users to add real-time events that are stored in the browser, ensuring the persistence of events.",
+      link: "https://cjsolis12.github.io/Work-Day-Schedular/",
+    },
+  ];
+
+  export default extraWork;
